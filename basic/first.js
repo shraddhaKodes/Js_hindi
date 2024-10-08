5f5dced4d0964334a709cf66335f392b585e17bc
shraddhaKodes <163244577+shraddhaKodes@users.noreply.github.com>
2024-10-08 08:25:01 +0000

    container config done

--- basic/first.js
+++ basic/first.js
@@ -1,1 +1,36 @@
 console.log("shraddha")
+//variable declaration
+const a = 100;
+let b = 10;
+b =  4;
+var c = 20;
+var c= 90;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//conversion.........................
+let A = "300";
+console.log(typeof A);
+A = Number(A);
+console.log(typeof A);
+
+let B = "300a";
+console.log(typeof B);
+B = Number(B);
+console.log(typeof B);
+console.log(B)
+// if number is 1 then  type cast in the boolean is to true if 0 is to false . if string is to "" it is false and if "jjji" is true 
+
+//operations..................
+console.log(2**6);
+console.log("1"+"2");
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+console.log(+true);
+
+let num1,num2,num3
+num1 = num2 = num3 = 2;
+console.log(++num1);
+console.log(num2++);
