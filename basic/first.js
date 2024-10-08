9e40a62f81053cd8243343380d47606ef836b084
shraddhaKodes <163244577+shraddhaKodes@users.noreply.github.com>
2024-10-08 08:25:19 +0000

    container config done

--- basic/first.js
+++ basic/first.js
@@ -34,3 +34,37 @@ let num1,num2,num3
 num1 = num2 = num3 = 2;
 console.log(++num1);
 console.log(num2++);
+
+//////////////////////////////////////stack(primitive) and heap(non - primitive);
+let n1 = 100;
+let n2 = n1;
+n2 = 300;
+console.log(n1);
+console.log(n2);
+
+let obj1 = {
+    name : "maya",
+    age : 30 
+}
+let obj2 = obj1 ;
+obj2.name = "manya";
+console.log(obj1.name);
+console.log(obj2.name);
+
+//all about string
+str = " hello myself Shraddha";
+console.log(str);
+console.log(str.substring(7,13));
+console.log(str.indexOf("myself"));
+console.log(str.trim());
+console.log(str.replace("Shraddha","sara"));
+str = new String("hi always try to be the best version of yourself");
+str1 = "he be the besyt version of yourself";
+console.log(str);
+console.log(str1.split())
+console.log(str1.split(" "))
+//some special charater - escape character"backward slash"
+str2 = "hi  try your 100% with your all effort because \"success\" is not  just piece of cake if it is like that then you never realised that what is the happiness of the truth and hardwork those people think it is cheaper but for some it is the ideal and necessary";
+console.log(str2);
+str3 = "hi be happy yaara ....because zindagi ni/ milegi /dubara don't be bechara enjoy your journey"
+console.log(str3)
